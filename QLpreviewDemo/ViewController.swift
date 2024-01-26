801ce92131f6f7ef21e4f965990eab59b0e85895
Faiz Ul Hassan <faiz.ul.hassan@sdsoltechnologies.com>
2024-01-26 17:40:25 +0500

    Initial Commit

---
+++ QLpreviewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QLpreviewDemo
+//
+//  Created by Faiz Ul Hassan on 26/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
